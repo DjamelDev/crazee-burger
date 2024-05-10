ec703b812c8335b1e08250a81a524a1e69a2cc4f
Djamel <djameldev@gmail.com>
2024-05-10 16:54:37 +0200

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginPage = () => {
+  const [prenom, setPrenom] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bonjour ${prenom}`);
+    setPrenom("");
+  };
+
+  const name = (params) => {
+    
+  }
+
   return (
     <div>
-      <p>LoginPage</p>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <br />
+      <h2>Connectez-vous</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Entrez votre prénom..."
+          required
+          value={prenom}
+          onChange={(e) => setPrenom(e.target.value)}
+        />
+        <button>Accédez à votre espace</button>
+      </form>
     </div>
   );
 };
