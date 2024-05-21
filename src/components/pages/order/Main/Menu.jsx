d0c342889f004557b0a6c2c45afe50d6d5fa8e20
Djamel <djameldev@gmail.com>
2024-05-21 23:47:22 +0200

    ♻️ F05: ORDER > MENU > Replace div to <Product/>

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
 import { theme } from "../../../../theme";
+import Product from "../Product";
 
 const Menu = () => {
   const [products, setProducts] = useState(fakeMenu2);
@@ -9,9 +10,12 @@ const Menu = () => {
   return (
     <MenuStyled>
       {products.map((product) => (
-        <div className="product" key={product.id}>
-          <img src={product.imageSource} alt={product.title} />
-        </div>
+        <Product
+          title={product.title}
+          imageSource={product.imageSource}
+          price={product.price}
+          key={product.id}
+        />
       ))}
     </MenuStyled>
   );
@@ -27,19 +31,4 @@ const MenuStyled = styled.div`
   grid-row-gap: 60px;
   padding: 50px 50px 150px;
   justify-items: center;
-
-  .product {
-    background: ${theme.colors.background_white};
-    width: 240px;
-    height: 330px;
-    border: 1px solid black;
-    box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
-
-    img {
-      width: 200px;
-      height: 145px;
-      object-fit: contain;
-      margin: 50px 20px 10px 20px;
-    }
-  }
 `;
