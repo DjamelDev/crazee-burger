18eda3e6683980cd0cce2c31cfcd7b47c4106426
Djamel <djameldev@gmail.com>
2024-06-09 17:56:06 +0200

    💄 F07: ADMIN > Add AdminTabs & AdminPanel

---
+++ src/components/pages/order/Admin/AdminPanel.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+import { theme } from "../../../../theme";
+
+const AdminPanel = () => {
+  return <AdminPanelStyled>AdminPanel</AdminPanelStyled>;
+};
+
+export default AdminPanel;
+
+const AdminPanelStyled = styled.div`
+  height: 250px;
+  background: ${theme.colors.white};
+  border: 1px solid ${theme.colors.greyLight};
+  box-shadow: ${theme.shadows.subtle};
+`;
