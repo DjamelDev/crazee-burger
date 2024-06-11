7fed787e73727ca3acffdc0ed4934fe86f3b4575
Djamel <djameldev@gmail.com>
2024-06-11 19:32:01 +0200

    ✨  F07: ADMIN > Tab > Add label

--- src/components/pages/order/Admin/AdminTabs.jsx
+++ src/components/pages/order/Admin/AdminTabs.jsx
@@ -1,13 +1,28 @@
 import styled from "styled-components";
 import Tab from "../../../reusable-ui/Tab.jsx";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { AiOutlinePlus } from "react-icons/ai";
+import { MdModeEditOutline } from "react-icons/md";
 import { theme } from "../../../../theme/index.js";
 
 const AdminTabs = ({ isCollapsed, setIsCollapsed }) => {
   return (
     <AdminTabsStyled>
       <Tab
+        label={""}
         Icon={isCollapsed ? <FiChevronUp /> : <FiChevronDown />}
+        onClick={() => setIsCollapsed(!isCollapsed)}
+        className={isCollapsed ? "is-active" : ""}
+      />
+      <Tab
+        label={"Ajouter un produit"}
+        Icon={<AiOutlinePlus />}
+        onClick={() => setIsCollapsed(!isCollapsed)}
+        className={isCollapsed ? "is-active" : ""}
+      />
+      <Tab
+        label={"Modifier un produit"}
+        Icon={<MdModeEditOutline />}
         onClick={() => setIsCollapsed(!isCollapsed)}
         className={isCollapsed ? "is-active" : ""}
       />
@@ -25,5 +40,9 @@ const AdminTabsStyled = styled.div`
     background: ${theme.colors.background_dark};
     color: ${theme.colors.white};
     border-color: ${theme.colors.background_dark};
+  }
+
+  button {
+    margin-left: 1px;
   }
 `;
